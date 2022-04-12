63cff8513f9b91f59c5532468f3644aeb1784c03
User <User@DESKTOP-J34PG4S>
2022-04-13 07:48:50 +0800

    加入專案檔案。

---
+++ Stack/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+  internal class Program
+  {
+    /// <summary>
+    /// Stack features: last in first out or first in last out.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+      int size = 5;
+      StackByArray stack = new StackByArray(size);
+      Console.WriteLine("Please enter {0} number data to stack:", size);
+
+      int value;
+      for (int i = 0; i < size; i++)
+      {
+        value = int.Parse(Console.ReadLine());
+        stack.push(value);
+      }
+
+      Console.WriteLine("\nStack pop up sequence:");
+
+      while (!stack.empty())
+      {
+        Console.WriteLine(stack.pop());
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Stack/StackByArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+  internal class StackByArray
+  {
+    // Member variable.
+    private int[] _stack;
+    private int _top;
+
+    // Constructors.
+    public StackByArray(int size)
+    {
+      _stack = new int[size]; // create array
+      _top = -1;              // it's means empty of stack
+    }
+
+    /// <summary>
+    /// When you push data into stack, check the stack is full or not.
+    /// </summary>
+    /// <param name="data">import data.</param>
+    /// <returns>true or false.</returns>
+    public bool push(int data)
+    {
+      if (_top >= _stack.Length)
+      {
+        Console.WriteLine("Stack is full!!");
+        return false;
+      }
+      else
+      {
+        _stack[++_top] = data;
+        return true;
+      }
+    }
+
+    /// <summary>
+    /// Check the stack is empty or not.
+    /// </summary>
+    /// <returns>true or false.</returns>
+    public bool empty()
+    {
+      return (_top == -1 ? true : false);
+    }
+
+    /// <summary>
+    /// When you pop up data from the stack, check the stack is empty or not.
+    /// </summary>
+    /// <returns>array index.</returns>
+    public int pop()
+    {
+      return (empty() ? -1 : _stack[_top--]);
+    }
+  }
+}
